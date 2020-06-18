6ecb66dba2cacf7a07e0ef344437bde38a2ffd97
unknown <mfarmer5102@gmail.com>
2020-06-17 22:45:21 -0400

    Continuing work on reusable table

--- src/components/StandardTable.vue
+++ src/components/StandardTable.vue
@@ -1,11 +1,15 @@
 <template>
     <div style="overflow-x: scroll; font-size: 14px">
         <table>
+
+
+
+            <!-- BEGIN Header -->
             <th :style='
                     "width: " +             "50px" +                                    ";" +
                     "text-align: " +        "center" +                                  ";" +
                     "color: " +             "white" +                                   ";" +
-                    "background-color: " +  "rgb(150,150,150)" +                        ";" +
+                    "background-color: " +  "rgb(175,175,175)" +                        ";" +
                     "border:" +             "1px solid rgba(210,210,210)" +             ";" +
                     "padding: " +           "8px" +                                     ";"
                 '
@@ -17,24 +21,48 @@
                     "width: " +             col.width +                                 ";" +
                     "text-align: " +        col.textAlignment +                         ";" +
                     "color: " +             "white" +                                   ";" +
-                    "background-color: " +  "rgb(150,150,150)" +                        ";" +
+                    "background-color: " +  "rgb(175,175,175)" +                        ";" +
                     "border:" +             "1px solid rgba(210,210,210)" +             ";" +
                     "padding: " +           "8px" +                                     ";"
                 '
             >
                 {{col.header}}
             </th>
+            <!-- END Header -->
+
+
+
+            <!-- BEGIN Filter Bar -->
+            <tr>
+                <td style="padding: 5px; background-color: rgb(205, 205, 205)"/>
+                <td v-for='(item, colIndex) in filterBar.filter(item => 1 == 1)' :key='colIndex + "filter"' style="padding: 5px; background-color: rgb(205, 205, 205)">
+                    <input v-if='item.type == "text"'
+                        v-on:keyup.enter="item.onPressEnter"
+                        type="text" 
+                        style="border: none; border-radius: 5px; padding: 8px; width: 100%;"
+                    />
+                    <select v-if='item.type == "select"' style="border: none; border-radius: 5px; padding: 8px; width: 100%;">
+                        <option v-for='(opt, optIndex) in item.options' :key='optIndex + "option"' :value='opt.value'>{{opt.label}}</option>
+                    </select>
+                </td>
+            </tr>
+            <!-- END Filter Bar-->
+
+
+
+            <!-- BEGIN Content -->
             <tr v-for='(row, rowIndex) in rows.filter(row => 1 == 1)'
-                @click='processClick(rowIndex)' 
                 :key='colIndex + rowIndex + "row"'
                 :style='
                     "background-color: " +  (rowIndex % 2 == 0 ? "white" : "rgba(230,230,230)") +           ";"
                 '
             >
-                <td :style='"border: 1px solid rgba(210,210,210); padding: 8px"'>{{rowIndex + 1}}</td>
+                <td :style='"border: 1px solid rgba(210,210,210); padding: 15px"'>{{rowIndex + 1}}</td>
                 <td v-for='(field, fieldIndex) in Object.keys(row).filter(field => !hiddenFields.includes(field))'
+                    @click='processClick(row._id)'
                     :key='colIndex + rowIndex + fieldIndex + "field"'
                     :title='row[field]'
+                    :v-html="row[field]"
                     :style='
                         "border: 1px solid rgba(210,210,210); " +
                         "padding: 8px; " +
@@ -44,17 +72,60 @@
                         "max-width: " + "200px" + ";"
                     '
                 >
-                    {{row[field]}}
+                    <span v-html='row[field]'></span>
                 </td>
             </tr>
+            <!-- END content -->
+
+
+
         </table>
     </div>
 </template>
 
 <script>
 
-// import ContactCard from "@/components/ContactCard.vue";
-// import NavbarContact from "@/components/Navbars/Contact.vue";
+var filterBar = [
+    {
+        type: 'text',
+        associatedDataField: 'name',
+        onPressEnter: function (e) {
+            event.preventDefault(e);
+            alert('enter pressed: ' + this.associatedDataField)
+        }
+    },
+    {
+        type: 'select',
+        options: [
+            {label: 'Language', value: 'language'},
+            {label: 'Front-End Framework', value: 'front-end-framework'},
+            {label: 'Back-End Framework', value: 'back-end-framework'},
+            {label: 'Library', value: 'library'},
+            {label: 'Database', value: 'database'},
+            {label: 'Deployment Technology', value: 'deployment'},
+            {label: 'ORM', value: 'orm'},
+            {label: 'Operating System', value: 'operating-system'},
+            {label: 'Other', value: 'other'},
+        ],
+        associatedDataField: 'type'
+    },
+    {
+        type: 'select',
+        options: [
+            {label: 'Yes', value: 'true'},
+            {label: 'No', value: 'false'},
+        ],
+        associatedDataField: 'showInGallery'
+    },
+    {
+        type: 'select',
+        options: [
+            {label: 'Yes', value: 'true'},
+            {label: 'No', value: 'false'},
+        ],
+        associatedDataField: 'showOnPortfolio'
+    }
+]
 var xyz = [
     {
         "isCollaboration": "",
@@ -154,94 +225,96 @@ var xyz = [
     }
 ]
 
+var abc = [
+    {
+        header: 'Title',
+        width: '200px',
+        textAlignment: 'Left',
+    },
+    {
+        header: 'Publish Date',
+        width: '150px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Title',
+        width: '300px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+                    {
+        header: 'Title',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '150px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Title',
+        width: '300px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+                    {
+        header: 'Title',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '150px',
+        textAlignment: 'Left'
+    },
+                    {
+        header: 'Title',
+        width: '300px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+                    {
+        header: 'Title',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Publish Date',
+        width: '150px',
+        textAlignment: 'Left'
+    }
+]
+
+var def = ['id', 'uuid']
+
 export default {
     name: 'StandardTable',
     props: [],
-    components: {
-        // ContactCard,
-        // NavbarContact
-    },
+    components: {},
     methods: {
-        processClick: function(id) {
-            alert(id)
+        processClick: function(data) {
+            alert(data)
         }
     },
     data: function () {
         return {
-            columns: [
-                {
-                    header: 'Title',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '150px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Title',
-                    width: '300px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                                {
-                    header: 'Title',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '150px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Title',
-                    width: '300px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                                {
-                    header: 'Title',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '150px',
-                    textAlignment: 'Left'
-                },
-                                {
-                    header: 'Title',
-                    width: '300px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                                {
-                    header: 'Title',
-                    width: '200px',
-                    textAlignment: 'Left'
-                },
-                {
-                    header: 'Publish Date',
-                    width: '150px',
-                    textAlignment: 'Left'
-                }
-            ],
-            rows: xyz,
-            hiddenFields: ['id', 'uuid']
+            columns: this.$attrs.columns || abc,
+            rows: this.$attrs.rows || xyz,
+            filterBar: this.$attrs.filterBar || filterBar,
+            hiddenFields: this.$attrs.hiddenFields || def
         }
     }
 }
--- src/views/Skills-Advanced.vue
+++ src/views/Skills-Advanced.vue
@@ -2,11 +2,16 @@
     <div>
         <NavbarSkills/>
         <section v-if="skillsLoaded" class='animated fadeIn'>
-            <table class="table table-striped">
+            <StandardTable
+                :columns='tableColumns'
+                :rows='allSkills'
+                :hiddenFields='["_id"]'
+            />
+            <!-- <table class="table table-striped">
                 <div v-for='(item, index) in allSkills.filter(item => 1 == 1)' :key='index'>
                     <SkillRow v-bind='item' :rownum='index'/>
                 </div>
-            </table>
+            </table> -->
         </section>
 
         <section v-else>
@@ -19,10 +24,35 @@
 <script>
 
 // @ is an alias to /src
+
+var z = [
+    {
+        header: 'Skill Name',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+                    {
+        header: 'Type',
+        width: '200px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Utilized by App',
+        width: '150px',
+        textAlignment: 'Left'
+    },
+    {
+        header: 'Display on Portfolio',
+        width: '150px',
+        textAlignment: 'Left'
+    }
+]
+
 import SkillPreview from "@/components/SkillPreview.vue";
 import SkillRow from "@/components/SkillRow.vue";
 import ScreenOverlay from "@/components/ScreenOverlay.vue";
 import NavbarSkills from "@/components/Navbars/Skills.vue";
+import StandardTable from "@/components/StandardTable.vue"
 import global from "@/global.js";
 
 export default {
@@ -31,19 +61,37 @@ export default {
         SkillPreview,
         SkillRow,
         ScreenOverlay,
-        NavbarSkills
+        NavbarSkills,
+        StandardTable
+    },
+    mounted: function() {
+        this.formatSkillsForTable();
+    },
+    methods: {
+        formatSkillsForTable() {
+            var self = this;
+            self.allSkills.forEach(skill => {
+                console.log('running')
+                var formattedSkill = skill;
+                skill.showInGallery ? formattedSkill.showInGallery = '<i class="fas fa-check fa-lg"></i>' : formattedSkill.showInGallery = '';
+                skill.showOnPortfolio ? formattedSkill.showOnPortfolio = '<i class="fas fa-check fa-lg"></i>' : formattedSkill.showOnPortfolio = '';
+                self.formattedSkills.push(formattedSkill)
+            })
+        }
     },
     data() {
         return {
             componentKey: 0,
             allSkills: global.skills,
+            formattedSkills: [],
             skillsLoaded: global.skillsLoaded,
             loadingMessage: `
                 <div>Loading list of skills...</div>
                 <div class="spinner-grow text-success" role="status">
                     <span class="sr-only">Loading...</span>
                 </div>
-            `
+            `,
+            tableColumns: z
         }
     }
 };
