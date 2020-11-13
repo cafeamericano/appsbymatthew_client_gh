e0b31fe5ce539fb4c9232d7fba24b8503a180f30
unknown <mfarmer5102@gmail.com>
2020-11-12 21:48:00 -0500

    updated api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     config: {
-        apiUrl: `http://192.168.86.22:5000/api` //'https://appsbymatthew-qgzgpr7klq-uc.a.run.app/api/'
+        apiUrl: `https://appsbymatthew-api-v5omtve52a-uc.a.run.app/api` //`http://192.168.86.22:5000/api`
     }
 }
