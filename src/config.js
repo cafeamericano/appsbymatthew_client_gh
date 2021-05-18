54bcf666fc22c5015e99334764840b9d81b916c8
Matthew Farmer <matthewfarmer@matthews-air.lan>
2021-05-17 21:30:21 -0400

    Logger url updated to production url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     config: {
         apiUrl: process.env.NODE_ENV == 'development' ?  'http://localhost:5000/api' : 'https://appsbymatthew-api-v5omtve52a-uc.a.run.app/api',
-        loggerUrl: 'http://localhost:9475/graphql'
+        loggerUrl: 'https://appsbymatthew-logs-v5omtve52a-uc.a.run.app/graphql'
     }
 }
