75307bb2447ea53de8fecbc32dd23d9a927bc3f9
Matthew Farmer <matthewfarmer@matthews-air.lan>
2021-05-17 21:13:27 -0400

    Able to capture ip address and browser type

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     config: {
         apiUrl: process.env.NODE_ENV == 'development' ?  'http://localhost:5000/api' : 'https://appsbymatthew-api-v5omtve52a-uc.a.run.app/api',
-        loggerUrl: 'https://schokolade.ue.r.appspot.com/api/useraction'
+        loggerUrl: 'http://localhost:9475/graphql'
     }
 }
