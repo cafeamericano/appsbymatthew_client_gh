8d0f637716ae1150546ba3f2683aa76e0e718733
Matthew Farmer <mfarmer5102@gmail.com>
2022-06-12 22:27:06 -0400

    Adjust logger to not record off-cloud implementations

--- src/analytics.js
+++ src/analytics.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const analyticsDashboardApiUrl = 'https://analytics-dashboard-api-bzqdzrhvba-uc.a.run.app';
+const hostnameSubjectToRecording = "appsbymatthew.com";
 
 const analytics = {
 
     recordAction: (reqBody) => {
-        if (process.env.NODE_ENV === "development") return;
+        if (!window.location.hostname.includes(hostnameSubjectToRecording));
         axios.post(analyticsDashboardApiUrl + "/api/action/record", reqBody)
             .then(response => {
                 if (response.ok) {
